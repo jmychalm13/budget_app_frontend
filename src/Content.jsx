5a837ca258b0ce9c89cc3172b51d94ceedb619e0
Jess Moore <104534587+jmychalm13@users.noreply.github.com>
2024-08-11 10:13:11 -0500

    route added for login and working

--- src/Content.jsx
+++ src/Content.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { IncomesIndex } from "./IncomesIndex";
 import { IncomesNew } from "./IncomesNew";
+import { Login } from "./Login";
 import { Routes, Route } from "react-router-dom";
 
 export function Content() {
@@ -26,13 +27,9 @@ export function Content() {
 
   return (
     <Routes>
+      <Route path="/login" element={<Login />} />
       <Route path="/" element={<IncomesIndex incomes={incomes} />} />
       <Route path="/incomes/new" element={<IncomesNew onCreateIncome={handleCreateIncome} />} />
     </Routes>
-    // <div>
-    //   <h1>Welcome to Savr</h1>
-    //   <IncomesNew onCreateIncome={handleCreateIncome} />
-    //   <IncomesIndex incomes={incomes} />
-    // </div>
   );
 }
