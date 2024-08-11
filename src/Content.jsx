eb16a9f70fc187a3d4619e30b551ae6060224bad
Jess Moore <104534587+jmychalm13@users.noreply.github.com>
2024-08-11 08:03:48 -0500

    incomes index action added and working

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,23 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { IncomesIndex } from "./IncomesIndex";
+
 export function Content() {
+  const [incomes, setIncomes] = useState([]);
+
+  const handleIndexIncomes = () => {
+    axios.get("http://localhost:3000/incomes.json").then((response) => {
+      console.log(response.data);
+      setIncomes(response.data);
+    });
+  };
+
+  useEffect(handleIndexIncomes, []);
+
   return (
     <div>
       <h1>Welcome to Savr</h1>
+      <IncomesIndex incomes={incomes} />
     </div>
   );
 }
--- src/IncomesIndex.jsx
+++ src/IncomesIndex.jsx
@@ -1,7 +1,13 @@
-export function IncomesIndex() {
+export function IncomesIndex(props) {
   return (
     <div>
       <h1>All Incomes</h1>
+      {props.incomes.map((income) => (
+        <div key={income.id}>
+          <h1>{income.source}</h1>
+          <h2>{income.amount}</h2>
+        </div>
+      ))}
     </div>
   );
 }
