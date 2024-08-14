ca84dc6adb46c4759b79b1cb282b1b1824431ba8
Jess Moore <104534587+jmychalm13@users.noreply.github.com>
2024-08-14 14:17:21 -0500

    rendering income and expense data

--- src/FinancialSummary.jsx
+++ src/FinancialSummary.jsx
@@ -18,8 +18,11 @@ export function FinancialSummary() {
         axios
           .get("http://localhost:3000/expenses.json")
           .then((response) => {
-            console.log(response);
+            console.log(response.data);
             setExpenses(response.data);
+          })
+          .catch((error) => {
+            console.error("Error fetching expenses", error);
           })
           .catch((error) => {
             console.error("Error fetching data", error);
@@ -27,11 +30,62 @@ export function FinancialSummary() {
       });
   };
 
+  let totalIncome = 0;
+  let totalExpenses = 0;
+
+  const findTotalIncome = (incomes) => {
+    incomes.map((income) => {
+      totalIncome += +income.amount;
+    });
+    return totalIncome;
+  };
+
+  const findTotalExpenses = (expenses) => {
+    expenses.map((expense) => {
+      totalExpenses += +expense.amount;
+    });
+    return totalExpenses;
+  };
+
   useEffect(handleFetchData, []);
 
   return (
     <div>
-      <h1>Here I am</h1>
+      <h1>Financial Summary</h1>
+      <div className="income-summary">
+        <h1>Income</h1>
+        {incomes.map((income) => (
+          <div key={income.id} className="d-flex justify-content-between">
+            <p>{income.source}</p>
+            <p>{income.amount}</p>
+          </div>
+        ))}
+        <div className="d-flex justify-content-between">
+          <p>
+            <strong>Total:</strong>
+          </p>
+          <p>
+            <strong>{findTotalIncome(incomes)}</strong>
+          </p>
+        </div>
+      </div>
+      <div className="expense-summary">
+        <h1>Expenses</h1>
+        {expenses.map((expense) => (
+          <div key={expense.id} className="d-flex justify-content-between">
+            <p>{expense.category}</p>
+            <p>{expense.amount}</p>
+          </div>
+        ))}
+        <div className="d-flex justify-content-between">
+          <p>
+            <strong>Total:</strong>
+          </p>
+          <p>
+            <strong>{findTotalExpenses(expenses)}</strong>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
