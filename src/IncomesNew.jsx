e69ffa3cfab5e9d4a23ae9cd4bf86df146825210
Jess Moore <104534587+jmychalm13@users.noreply.github.com>
2024-08-14 17:03:12 -0500

    changed addIncome to only accept set income sources

--- src/IncomesNew.jsx
+++ src/IncomesNew.jsx
@@ -1,7 +1,12 @@
 /* eslint-disable react/prop-types */
 // import axios from "axios";
+import { useState } from "react";
 
 export function IncomesNew(props) {
+  const incomeSources = ["salary", "freelance", "investment"];
+
+  const [selectedIncomeSource, setSelectedIncomeSource] = useState("");
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const params = new FormData(event.target);
@@ -13,12 +18,25 @@ export function IncomesNew(props) {
       <div className="card bg-success-subtle p-4">
         <h1 className="text-center">New Income</h1>
         <form onSubmit={handleSubmit}>
-          <div className="row mb-3">
-            <label htmlFor="source" className="col-sm-2 col-form-label">
-              Source
-            </label>
-            <div className="col-sm-10">
-              <input type="text" className="form-control" id="source" name="source" placeholder="Enter income source" />
+          <div className="d-flex justify-content-between mb-3">
+            <div className="">
+              <label htmlFor="source">Source:</label>
+            </div>
+            <div className="">
+              <select
+                className="form-select form-select-sm mb-3 custom-width"
+                aria-label=".form-select-sm"
+                name="source"
+                id="source"
+                value={selectedIncomeSource}
+                onChange={(e) => setSelectedIncomeSource(e.target.value)}
+              >
+                {incomeSources.map((source) => (
+                  <option value={source} key={source}>
+                    {source}
+                  </option>
+                ))}
+              </select>
             </div>
           </div>
           <div className="row mb-3">
@@ -28,6 +46,7 @@ export function IncomesNew(props) {
             <div className="col-sm-10">
               <input
                 type="number"
+                step="0.01"
                 className="form-control"
                 id="amount"
                 name="amount"
